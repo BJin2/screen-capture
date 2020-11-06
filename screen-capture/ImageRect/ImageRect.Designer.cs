3bbe66bb8ddb8a980d43ed73c5784c8c4c8e3e5b
Jin <bomjan@naver.com>
2020-11-06 13:37:55 +0900

    Image rect ui elements

--- screen-capture/ImageRect/ImageRect.Designer.cs
+++ screen-capture/ImageRect/ImageRect.Designer.cs
@@ -28,22 +28,43 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ImageRect));
 			this.panel5 = new System.Windows.Forms.Panel();
-			this.bottomLeft = new System.Windows.Forms.Panel();
-			this.bottomRight = new System.Windows.Forms.Panel();
 			this.bottom = new System.Windows.Forms.Panel();
+			this.bottomRight = new System.Windows.Forms.Panel();
+			this.bottomLeft = new System.Windows.Forms.Panel();
 			this.panel1 = new System.Windows.Forms.Panel();
-			this.topLeft = new System.Windows.Forms.Panel();
+			this.top = new System.Windows.Forms.Panel();
 			this.topRight = new System.Windows.Forms.Panel();
+			this.topLeft = new System.Windows.Forms.Panel();
 			this.captureArea = new System.Windows.Forms.Panel();
 			this.borderPanel = new System.Windows.Forms.Panel();
-			this.titlePanel = new System.Windows.Forms.Panel();
-			this.top = new System.Windows.Forms.Panel();
-			this.left = new System.Windows.Forms.Panel();
 			this.right = new System.Windows.Forms.Panel();
+			this.left = new System.Windows.Forms.Panel();
+			this.titlePanel = new System.Windows.Forms.Panel();
+			this.textArea = new System.Windows.Forms.Panel();
+			this.pos = new System.Windows.Forms.Panel();
+			this.panel3 = new System.Windows.Forms.Panel();
+			this.coordX = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.coordY = new System.Windows.Forms.TextBox();
+			this.res = new System.Windows.Forms.Panel();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.panel4 = new System.Windows.Forms.Panel();
+			this.label4 = new System.Windows.Forms.Label();
+			this.captureButton = new System.Windows.Forms.Button();
 			this.panel5.SuspendLayout();
 			this.panel1.SuspendLayout();
 			this.borderPanel.SuspendLayout();
+			this.titlePanel.SuspendLayout();
+			this.textArea.SuspendLayout();
+			this.pos.SuspendLayout();
+			this.panel3.SuspendLayout();
+			this.res.SuspendLayout();
+			this.panel4.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// panel5
@@ -52,20 +73,20 @@
 			this.panel5.Controls.Add(this.bottomRight);
 			this.panel5.Controls.Add(this.bottomLeft);
 			this.panel5.Dock = System.Windows.Forms.DockStyle.Bottom;
-			this.panel5.Location = new System.Drawing.Point(0, 410);
+			this.panel5.Location = new System.Drawing.Point(0, 395);
 			this.panel5.Name = "panel5";
 			this.panel5.Size = new System.Drawing.Size(800, 5);
 			this.panel5.TabIndex = 6;
 			// 
-			// bottomLeft
+			// bottom
 			// 
-			this.bottomLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-			this.bottomLeft.Cursor = System.Windows.Forms.Cursors.SizeNESW;
-			this.bottomLeft.Dock = System.Windows.Forms.DockStyle.Left;
-			this.bottomLeft.Location = new System.Drawing.Point(0, 0);
-			this.bottomLeft.Name = "bottomLeft";
-			this.bottomLeft.Size = new System.Drawing.Size(5, 5);
-			this.bottomLeft.TabIndex = 0;
+			this.bottom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.bottom.Cursor = System.Windows.Forms.Cursors.SizeNS;
+			this.bottom.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.bottom.Location = new System.Drawing.Point(5, 0);
+			this.bottom.Name = "bottom";
+			this.bottom.Size = new System.Drawing.Size(790, 5);
+			this.bottom.TabIndex = 2;
 			// 
 			// bottomRight
 			// 
@@ -77,15 +98,15 @@
 			this.bottomRight.Size = new System.Drawing.Size(5, 5);
 			this.bottomRight.TabIndex = 1;
 			// 
-			// bottom
+			// bottomLeft
 			// 
-			this.bottom.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-			this.bottom.Cursor = System.Windows.Forms.Cursors.SizeNS;
-			this.bottom.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.bottom.Location = new System.Drawing.Point(5, 0);
-			this.bottom.Name = "bottom";
-			this.bottom.Size = new System.Drawing.Size(790, 5);
-			this.bottom.TabIndex = 2;
+			this.bottomLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.bottomLeft.Cursor = System.Windows.Forms.Cursors.SizeNESW;
+			this.bottomLeft.Dock = System.Windows.Forms.DockStyle.Left;
+			this.bottomLeft.Location = new System.Drawing.Point(0, 0);
+			this.bottomLeft.Name = "bottomLeft";
+			this.bottomLeft.Size = new System.Drawing.Size(5, 5);
+			this.bottomLeft.TabIndex = 0;
 			// 
 			// panel1
 			// 
@@ -98,15 +119,15 @@
 			this.panel1.Size = new System.Drawing.Size(800, 5);
 			this.panel1.TabIndex = 5;
 			// 
-			// topLeft
+			// top
 			// 
-			this.topLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-			this.topLeft.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
-			this.topLeft.Dock = System.Windows.Forms.DockStyle.Left;
-			this.topLeft.Location = new System.Drawing.Point(0, 0);
-			this.topLeft.Name = "topLeft";
-			this.topLeft.Size = new System.Drawing.Size(5, 5);
-			this.topLeft.TabIndex = 0;
+			this.top.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.top.Cursor = System.Windows.Forms.Cursors.SizeNS;
+			this.top.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.top.Location = new System.Drawing.Point(5, 0);
+			this.top.Name = "top";
+			this.top.Size = new System.Drawing.Size(790, 5);
+			this.top.TabIndex = 2;
 			// 
 			// topRight
 			// 
@@ -118,12 +139,22 @@
 			this.topRight.Size = new System.Drawing.Size(5, 5);
 			this.topRight.TabIndex = 1;
 			// 
+			// topLeft
+			// 
+			this.topLeft.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(215)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+			this.topLeft.Cursor = System.Windows.Forms.Cursors.SizeNWSE;
+			this.topLeft.Dock = System.Windows.Forms.DockStyle.Left;
+			this.topLeft.Location = new System.Drawing.Point(0, 0);
+			this.topLeft.Name = "topLeft";
+			this.topLeft.Size = new System.Drawing.Size(5, 5);
+			this.topLeft.TabIndex = 0;
+			// 
 			// captureArea
 			// 
 			this.captureArea.Dock = System.Windows.Forms.DockStyle.Fill;
 			this.captureArea.Location = new System.Drawing.Point(0, 5);
 			this.captureArea.Name = "captureArea";
-			this.captureArea.Size = new System.Drawing.Size(800, 405);
+			this.captureArea.Size = new System.Drawing.Size(800, 390);
 			this.captureArea.TabIndex = 9;
 			// 
 			// borderPanel
@@ -134,39 +165,10 @@
 			this.borderPanel.Controls.Add(this.panel1);
 			this.borderPanel.Controls.Add(this.panel5);
 			this.borderPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.borderPanel.Location = new System.Drawing.Point(0, 35);
+			this.borderPanel.Location = new System.Drawing.Point(0, 50);
 			this.borderPanel.Name = "borderPanel";
-			this.borderPanel.Size = new System.Drawing.Size(800, 415);
+			this.borderPanel.Size = new System.Drawing.Size(800, 400);
 			this.borderPanel.TabIndex = 1;
-			// 
-			// titlePanel
-			// 
-			this.titlePanel.BackColor = System.Drawing.SystemColors.ControlDark;
-			this.titlePanel.Dock = System.Windows.Forms.DockStyle.Top;
-			this.titlePanel.Location = new System.Drawing.Point(0, 0);
-			this.titlePanel.Name = "titlePanel";
-			this.titlePanel.Size = new System.Drawing.Size(800, 35);
-			this.titlePanel.TabIndex = 0;
-			// 
-			// top
-			// 
-			this.top.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-			this.top.Cursor = System.Windows.Forms.Cursors.SizeNS;
-			this.top.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.top.Location = new System.Drawing.Point(5, 0);
-			this.top.Name = "top";
-			this.top.Size = new System.Drawing.Size(790, 5);
-			this.top.TabIndex = 2;
-			// 
-			// left
-			// 
-			this.left.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
-			this.left.Cursor = System.Windows.Forms.Cursors.SizeWE;
-			this.left.Dock = System.Windows.Forms.DockStyle.Left;
-			this.left.Location = new System.Drawing.Point(0, 5);
-			this.left.Name = "left";
-			this.left.Size = new System.Drawing.Size(5, 405);
-			this.left.TabIndex = 10;
 			// 
 			// right
 			// 
@@ -175,8 +177,189 @@
 			this.right.Dock = System.Windows.Forms.DockStyle.Right;
 			this.right.Location = new System.Drawing.Point(795, 5);
 			this.right.Name = "right";
-			this.right.Size = new System.Drawing.Size(5, 405);
+			this.right.Size = new System.Drawing.Size(5, 390);
 			this.right.TabIndex = 11;
+			// 
+			// left
+			// 
+			this.left.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+			this.left.Cursor = System.Windows.Forms.Cursors.SizeWE;
+			this.left.Dock = System.Windows.Forms.DockStyle.Left;
+			this.left.Location = new System.Drawing.Point(0, 5);
+			this.left.Name = "left";
+			this.left.Size = new System.Drawing.Size(5, 390);
+			this.left.TabIndex = 10;
+			// 
+			// titlePanel
+			// 
+			this.titlePanel.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.titlePanel.Controls.Add(this.captureButton);
+			this.titlePanel.Controls.Add(this.textArea);
+			this.titlePanel.Dock = System.Windows.Forms.DockStyle.Top;
+			this.titlePanel.Location = new System.Drawing.Point(0, 0);
+			this.titlePanel.Name = "titlePanel";
+			this.titlePanel.Size = new System.Drawing.Size(800, 50);
+			this.titlePanel.TabIndex = 0;
+			// 
+			// textArea
+			// 
+			this.textArea.Controls.Add(this.res);
+			this.textArea.Controls.Add(this.pos);
+			this.textArea.Dock = System.Windows.Forms.DockStyle.Left;
+			this.textArea.Location = new System.Drawing.Point(0, 0);
+			this.textArea.Name = "textArea";
+			this.textArea.Size = new System.Drawing.Size(180, 50);
+			this.textArea.TabIndex = 0;
+			// 
+			// pos
+			// 
+			this.pos.Controls.Add(this.coordY);
+			this.pos.Controls.Add(this.label2);
+			this.pos.Controls.Add(this.coordX);
+			this.pos.Controls.Add(this.panel3);
+			this.pos.Dock = System.Windows.Forms.DockStyle.Top;
+			this.pos.Location = new System.Drawing.Point(0, 0);
+			this.pos.Name = "pos";
+			this.pos.Padding = new System.Windows.Forms.Padding(5, 2, 0, 0);
+			this.pos.Size = new System.Drawing.Size(180, 25);
+			this.pos.TabIndex = 0;
+			// 
+			// panel3
+			// 
+			this.panel3.Controls.Add(this.label1);
+			this.panel3.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel3.Location = new System.Drawing.Point(5, 2);
+			this.panel3.Name = "panel3";
+			this.panel3.Size = new System.Drawing.Size(60, 23);
+			this.panel3.TabIndex = 0;
+			// 
+			// coordX
+			// 
+			this.coordX.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.coordX.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.coordX.Dock = System.Windows.Forms.DockStyle.Left;
+			this.coordX.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.coordX.Location = new System.Drawing.Point(65, 2);
+			this.coordX.Name = "coordX";
+			this.coordX.Size = new System.Drawing.Size(49, 22);
+			this.coordX.TabIndex = 1;
+			this.coordX.Text = "00000";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label1.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label1.Location = new System.Drawing.Point(0, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(56, 21);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Coord";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label2.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label2.Location = new System.Drawing.Point(114, 2);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(14, 21);
+			this.label2.TabIndex = 2;
+			this.label2.Text = ",";
+			// 
+			// coordY
+			// 
+			this.coordY.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.coordY.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.coordY.Dock = System.Windows.Forms.DockStyle.Right;
+			this.coordY.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.coordY.Location = new System.Drawing.Point(131, 2);
+			this.coordY.Name = "coordY";
+			this.coordY.Size = new System.Drawing.Size(49, 22);
+			this.coordY.TabIndex = 3;
+			this.coordY.Text = "00000";
+			// 
+			// res
+			// 
+			this.res.Controls.Add(this.textBox1);
+			this.res.Controls.Add(this.label3);
+			this.res.Controls.Add(this.textBox2);
+			this.res.Controls.Add(this.panel4);
+			this.res.Dock = System.Windows.Forms.DockStyle.Top;
+			this.res.Location = new System.Drawing.Point(0, 25);
+			this.res.Name = "res";
+			this.res.Padding = new System.Windows.Forms.Padding(5, 2, 0, 0);
+			this.res.Size = new System.Drawing.Size(180, 25);
+			this.res.TabIndex = 1;
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.Dock = System.Windows.Forms.DockStyle.Right;
+			this.textBox1.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.textBox1.Location = new System.Drawing.Point(131, 2);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(49, 22);
+			this.textBox1.TabIndex = 3;
+			this.textBox1.Text = "00000";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label3.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label3.Location = new System.Drawing.Point(114, 2);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(17, 21);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "x";
+			// 
+			// textBox2
+			// 
+			this.textBox2.BackColor = System.Drawing.SystemColors.ControlDark;
+			this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox2.Dock = System.Windows.Forms.DockStyle.Left;
+			this.textBox2.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.textBox2.Location = new System.Drawing.Point(65, 2);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(49, 22);
+			this.textBox2.TabIndex = 1;
+			this.textBox2.Text = "00000";
+			// 
+			// panel4
+			// 
+			this.panel4.Controls.Add(this.label4);
+			this.panel4.Dock = System.Windows.Forms.DockStyle.Left;
+			this.panel4.Location = new System.Drawing.Point(5, 2);
+			this.panel4.Name = "panel4";
+			this.panel4.Size = new System.Drawing.Size(60, 23);
+			this.panel4.TabIndex = 0;
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.label4.Font = new System.Drawing.Font("Malgun Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label4.Location = new System.Drawing.Point(0, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(36, 21);
+			this.label4.TabIndex = 0;
+			this.label4.Text = "Res";
+			// 
+			// captureButton
+			// 
+			this.captureButton.Dock = System.Windows.Forms.DockStyle.Right;
+			this.captureButton.FlatAppearance.BorderSize = 0;
+			this.captureButton.FlatAppearance.MouseDownBackColor = System.Drawing.SystemColors.ControlDarkDark;
+			this.captureButton.FlatAppearance.MouseOverBackColor = System.Drawing.SystemColors.ActiveCaption;
+			this.captureButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.captureButton.Image = ((System.Drawing.Image)(resources.GetObject("captureButton.Image")));
+			this.captureButton.Location = new System.Drawing.Point(750, 0);
+			this.captureButton.Name = "captureButton";
+			this.captureButton.Size = new System.Drawing.Size(50, 50);
+			this.captureButton.TabIndex = 1;
+			this.captureButton.UseVisualStyleBackColor = true;
 			// 
 			// ImageRect
 			// 
@@ -190,6 +373,7 @@
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
 			this.MaximizeBox = false;
 			this.MinimizeBox = false;
+			this.MinimumSize = new System.Drawing.Size(230, 60);
 			this.Name = "ImageRect";
 			this.ShowInTaskbar = false;
 			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
@@ -198,6 +382,16 @@
 			this.panel5.ResumeLayout(false);
 			this.panel1.ResumeLayout(false);
 			this.borderPanel.ResumeLayout(false);
+			this.titlePanel.ResumeLayout(false);
+			this.textArea.ResumeLayout(false);
+			this.pos.ResumeLayout(false);
+			this.pos.PerformLayout();
+			this.panel3.ResumeLayout(false);
+			this.panel3.PerformLayout();
+			this.res.ResumeLayout(false);
+			this.res.PerformLayout();
+			this.panel4.ResumeLayout(false);
+			this.panel4.PerformLayout();
 			this.ResumeLayout(false);
 
 		}
@@ -216,5 +410,19 @@
 		private System.Windows.Forms.Panel right;
 		private System.Windows.Forms.Panel left;
 		private System.Windows.Forms.Panel titlePanel;
+		private System.Windows.Forms.Panel textArea;
+		private System.Windows.Forms.Panel res;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Panel pos;
+		private System.Windows.Forms.TextBox coordY;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.TextBox coordX;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button captureButton;
 	}
 }
